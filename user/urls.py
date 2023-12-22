a34cc1ec63658ec7db3062d5efcf9288176c6b74
Jithin Manilal <jithinmanilal@gmail.com>
2023-12-22 16:20:36 +0530

    docstrings added

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import HomeView, CustomLoginView, DasboardView, CustomRegistrationView
+from .views import HomeView, CustomLoginView, DashboardView, CustomRegistrationView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('dashboard/', DasboardView.as_view(), name='dashboard'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', CustomRegistrationView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
--- user/views.py
+++ user/views.py
@@ -13,22 +13,35 @@ from django.views.decorators.cache import never_cache
 n_cache = [never_cache,]
 
 class HomeView(TemplateView):
+    """
+        Static view for landing page.
+    """
     template_name = 'user/index.html'
 
 @method_decorator(n_cache, name='dispatch')
-class DasboardView(LoginRequiredMixin, TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
+    """
+        Dashboard view for authenticated users.
+    """
     template_name = 'user/dashboard.html'
 
 class CustomLoginView(LoginView):
+    """
+        View for handling authentication.
+    """
     template_name = 'user/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
 
     def get_success_url(self):
+        # on authentication success the user is redirected
         return reverse_lazy('dashboard')
     
 
 class CustomRegistrationView(FormView):
+    """
+        View for registering a user.
+    """
     template_name = 'user/register.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('dashboard')
@@ -36,11 +49,13 @@ class CustomRegistrationView(FormView):
     def form_valid(self, form):
         user = form.save()
         if user:
+            # if user is saved then they are logged in.
             login(self.request, user)
         return super().form_valid(form)
     
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
+            # redirecting authenticated user away from the regitration page.
             return redirect('dashboard')
         return super(CustomRegistrationView, self).get(*args, **kwargs)
     
